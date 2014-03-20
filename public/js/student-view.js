96a139221033b1c9ed4389603c535cdce18e6326
Jason51122 <jason51122@hotmail.com>
2014-03-20 19:32:44 -0400

    Set the calendar in student-view to read info from Google calendar

--- public/js/student-view.js
+++ public/js/student-view.js
@@ -1,52 +1,22 @@
 $(document).ready(function() {
+	$('#calendar').fullCalendar('today');
+		
 	$('#calendar').fullCalendar({
-		height: 700,
-		aspectRatio: 1,
 		header: {
 			left: 'prev,next today',
 			center: 'title',
-			right: 'month,basicWeek,basicDay'
+			right: 'month,agendaWeek,agendaDay'
 		},
-		defaultDate: '2014-01-12',
-		editable: true,
-		events: [
-			{
-				title: 'All Day Event',
-				start: '2014-01-01'
-			},
-			{
-				title: 'Long Event',
-				start: '2014-01-07',
-				end: '2014-01-10'
-			},
-			{
-				id: 999,
-				title: 'Repeating Event',
-				start: '2014-01-09T16:00:00'
-			},
-			{
-				id: 999,
-				title: 'Repeating Event',
-				start: '2014-01-16T16:00:00'
-			},
-			{
-				title: 'Meeting',
-				start: '2014-01-12T10:30:00',
-				end: '2014-01-12T12:30:00'
-			},
-			{
-				title: 'Lunch',
-				start: '2014-01-12T12:00:00'
-			},
-			{
-				title: 'Birthday Party',
-				start: '2014-01-13T07:00:00'
-			},
-			{
-				title: 'Click for Google',
-				url: 'http://google.com/',
-				start: '2014-01-28'
-			}
-		]
+		defaultView: 'agendaWeek',
+		editable: false,
+		events: 'http://www.google.com/calendar/feeds/brownipp%40gmail.com/public/basic',
+		eventClick: function(event) {
+			// opens events in a popup window
+			window.open(event.url, 'gcalevent', 'width=700,height=600');
+			return false;
+		},
+		loading: function(bool) {
+			$('#loading').toggle(bool);
+		}
 	});
 });
