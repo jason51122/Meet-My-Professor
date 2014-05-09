624aa31cdcc9fd57305ba614dd563fbec8516d05
Zhixiong Chen <zx_chen@cs.brown.edu>
2014-05-09 15:05:05 -0400

    brown mx

---
+++ lib/emailer/index.js.cs
@@ -0,0 +1,37 @@
+var nodemailer = require("nodemailer");
+
+// create reusable transport method (opens pool of SMTP connections)
+var smtpTransport = nodemailer.createTransport("SMTP",{
+    host: "mx.cs.brown.edu",
+    port: 25,
+    auth: {
+        user: "meet.my.professor@gmail.com",
+        pass: "meetmyprofessor"
+    }
+});
+
+/* setup e-mail data with unicode symbols
+var mailOptions = {
+    from: "meet.my.professor@gmail.com", // sender address
+    to: "zhixiong_chen@brown.edu", // list of receivers
+    subject: "Hello", // Subject line
+    text: "Hello world" // plaintext body
+} */
+
+function send(mailOptions, callBack){
+
+	// send mail with defined transport object
+	smtpTransport.sendMail(mailOptions, function(error, response){
+	    if(error){
+	        console.log(error);
+	        callBack(error);
+	    }else{
+	        console.log("Message sent: " + response.message);
+	        callBack(null);
+	    }
+	    // if you don't want to use this transport object anymore, uncomment following line
+	    //smtpTransport.close(); // shut down the connection pool, no more messages
+	});
+}
+
+exports.send = send;
